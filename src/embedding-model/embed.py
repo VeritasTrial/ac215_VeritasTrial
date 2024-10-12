48f16db2f0a7542b8c8a6af0ddc24888be69d5d5
Charlie-XIAO <yx2436@nyu.edu>
2024-10-12 18:10:44 -0400

    comments

--- src/embedding-model/embed.py
+++ src/embedding-model/embed.py
@@ -2,6 +2,7 @@
 
 import chromadb
 import rich
+from google.cloud import storage
 
 from shared import (
     CHROMADB_COLLECTION_NAME,
@@ -10,16 +11,22 @@ from shared import (
     default_progress,
 )
 
+BUCKET_NAME = "veritas-trial"
+BUCKET_CLEANED_JSONL_PATH = "data-pipeline/cleaned_data.jsonl"
+
 
 def main():
-    client = chromadb.HttpClient(host=CHROMADB_HOST, port=CHROMADB_PORT)
-    collection = client.get_or_create_collection(CHROMADB_COLLECTION_NAME)
-
-    # TODO: fetch data from GCS
+    # Connect to the GCS bucket
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(BUCKET_NAME)
+    # TODO: fetch data from the bucket
 
     # TODO: create embeddings with BGE-M3
 
-    # TODO: upsert embeddings to ChromaDB
+    # Store embeddings in the ChromaDB vector database
+    client = chromadb.HttpClient(host=CHROMADB_HOST, port=CHROMADB_PORT)
+    collection = client.get_or_create_collection(CHROMADB_COLLECTION_NAME)
+    # TODO: upsert embeddings to the database
     # collection.upsert(
     #     ids=["id1"],
     #     embeddings=[[1, 2, 3]],
@@ -28,6 +35,6 @@ def main():
     # )
 
     rich.print(
-        f"[bold green]->[/] Embeddings stored in collection {CHROMADB_COLLECTION_NAME} "
-        "in ChromaDB."
+        "[bold green]->[/] Embeddings stored in collection "
+        f"{CHROMADB_COLLECTION_NAME!r} in ChromaDB."
     )
