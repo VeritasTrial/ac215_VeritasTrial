8d2afe535d6386cb8492e10ea31984bcda76b893
Charlie-XIAO <yx2436@nyu.edu>
2024-10-18 17:47:05 -0400

    prepend context to instruction

--- src/construct-qa/prepare.py
+++ src/construct-qa/prepare.py
@@ -4,63 +4,86 @@ import json
 
 import jsonlines
 import rich
-from sklearn.model_selection import train_test_split
 
 from shared import (
-    INSTRUCTION_TEST_JSONL_PATH,
-    INSTRUCTION_TRAIN_JSONL_PATH,
+    INSTRUCTION_JSONL_PATH,
     QA_JSON_PATH,
     default_progress,
+    get_cleaned_data,
+)
+
+# The template for the questions in the instruction dataset
+QUESTION_TEMPLATE = (
+    "Please read the following clinical trial information, then answer a question.\n\n"
+    "{study_repr}\n\n"
+    "### Question\n\n"
+    "{question}"
 )
 
 
-def main(seed):
+def get_study_repr(study):
+    """Get the text representation of a study to be prepended to the question."""
+    cleaned_study = study.copy()
+
+    # Remove irrelevant fields that may interfere with the instruction
+    cleaned_study.pop("id")
+    cleaned_study.pop("references")
+    cleaned_study.pop("documents")
+
+    return json.dumps(cleaned_study, indent=2)
+
+
+def main():
     if not QA_JSON_PATH.exists():
         rich.print(
             f"[bold red]Error:[/] Generated QA missing at: {QA_JSON_PATH}; run the "
-            "generate subcommand first"
+            "fetch or generate subcommands first"
         )
         return
 
-    # Load the generated QA JSON file
-    with QA_JSON_PATH.open("r", encoding="utf-8") as f:
-        qa_data = json.load(f)
-
-    # Convert into instruction dataset format
-    # {
-    #   "contents": [
-    #     {"role": "user", "parts": [{"text": "QUESTION?"}]},
-    #     {"role": "model", "parts": [{"text": "ANSWER."}]}
-    #   ]
-    # }
-    # https://cloud.google.com/vertex-ai/generative-ai/docs/models/gemini-supervised-tuning-prepare#dataset_example_for_gemini-15-pro_and_gemini-15-flash
     with default_progress() as progress:
+        # Load the cleaned data to pair with the generated QA
+        task = progress.add_task("Loading cleaned data...", total=1)
+        studies = get_cleaned_data()
+        progress.update(task, advance=1)
+
+        # Load the generated QA
+        task = progress.add_task("Loading generated QA...", total=1)
+        with QA_JSON_PATH.open("r", encoding="utf-8") as f:
+            qa_data = json.load(f)
+        progress.update(task, advance=1)
+
+        # Convert into instruction dataset format
+        # {
+        #   "contents": [
+        #     {"role": "user", "parts": [{"text": "QUESTION?"}]},
+        #     {"role": "model", "parts": [{"text": "ANSWER."}]}
+        #   ]
+        # }
+        # https://cloud.google.com/vertex-ai/generative-ai/docs/models/gemini-supervised-tuning-prepare#dataset_example_for_gemini-15-pro_and_gemini-15-flash
         task = progress.add_task("Construction dataset...", total=len(qa_data))
-        dataset = []
-        for _, qa_pairs in qa_data.items():
-            for qa_pair in qa_pairs:
-                dataset.append(
-                    {
-                        "contents": [
-                            {"role": "user", "parts": [{"text": qa_pair["question"]}]},
-                            {"role": "model", "parts": [{"text": qa_pair["answer"]}]},
-                        ],
-                    }
-                )
-            progress.update(task, advance=1)
+        n_written = 0
+        with jsonlines.open(INSTRUCTION_JSONL_PATH, "w") as f:
+            for nctid, qa_pairs in qa_data.items():
+                # Find the corresponding study from cleaned data
+                target_study = next(item for item in studies if item["id"] == nctid)
+                target_study_repr = get_study_repr(target_study)
 
-    # Split into training and testing sets
-    trainset, testset = train_test_split(dataset, test_size=0.1, random_state=seed)
-    with jsonlines.open(INSTRUCTION_TRAIN_JSONL_PATH, "w") as f:
-        f.write_all(trainset)
-    with jsonlines.open(INSTRUCTION_TEST_JSONL_PATH, "w") as f:
-        f.write_all(testset)
+                for qa_pair in qa_pairs:
+                    # The question will be combined with the study information to form
+                    # the instruction, and the answer will be kept as is
+                    formatted_question = QUESTION_TEMPLATE.format(
+                        study_repr=target_study_repr, question=qa_pair["question"]
+                    )
+                    conversation = [
+                        {"role": "user", "parts": [{"text": formatted_question}]},
+                        {"role": "model", "parts": [{"text": qa_pair["answer"]}]},
+                    ]
+                    f.write({"contents": conversation})
+                    n_written += 1
+                progress.update(task, advance=1)
 
     rich.print(
-        f"[bold green]->[/] {len(trainset)} training data saved to "
-        f"{INSTRUCTION_TRAIN_JSONL_PATH}"
-    )
-    rich.print(
-        f"[bold green]->[/] {len(testset)} testing data saved to "
-        f"{INSTRUCTION_TEST_JSONL_PATH}"
+        f"[bold green]->[/] {n_written} instruction data saved to "
+        f"{INSTRUCTION_JSONL_PATH}"
     )
--- src/construct-qa/shared.py
+++ src/construct-qa/shared.py
@@ -2,6 +2,8 @@
 
 from pathlib import Path
 
+import jsonlines
+from google.cloud import storage
 from rich.progress import (
     MofNCompleteColumn,
     Progress,
@@ -14,13 +16,11 @@ DATA_DIR = Path(__file__).parent / "data"
 ERRORS_DIR = DATA_DIR / "errors"
 CLEANED_JSONL_PATH = DATA_DIR / "cleaned_data.jsonl"
 QA_JSON_PATH = DATA_DIR / "generated_qa.json"
-INSTRUCTION_TRAIN_JSONL_PATH = DATA_DIR / "instruction_train.jsonl"
-INSTRUCTION_TEST_JSONL_PATH = DATA_DIR / "instruction_test.jsonl"
+INSTRUCTION_JSONL_PATH = DATA_DIR / "instruction.jsonl"
 
 BUCKET_NAME = "veritas-trial"
 BUCKET_QA_JSON_PATH = "construct-qa/generated_qa.json"
-BUCKET_INSTRUCTION_TRAIN_JSONL_PATH = "construct-qa/instruction_train.jsonl"
-BUCKET_INSTRUCTION_TEST_JSONL_PATH = "construct-qa/instruction_test.jsonl"
+BUCKET_INSTRUCTION_JSONL_PATH = "construct-qa/instruction.jsonl"
 
 if not DATA_DIR.exists():
     DATA_DIR.mkdir()
@@ -32,6 +32,22 @@ BUCKET_NAME = "veritas-trial"
 BUCKET_CLEANED_JSONL_PATH = "data-pipeline/cleaned_data.jsonl"
 
 GENERATIVE_MODEL = "gemini-1.5-flash-001"
+
+
+def get_cleaned_data():
+    """Get the cleaned data from the bucket or reuse from local."""
+    if not CLEANED_JSONL_PATH.exists():
+        # Fetch cleaned data from the bucket and store locally if is not already
+        # locally available
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(BUCKET_NAME)
+        data_blob = bucket.get_blob(BUCKET_CLEANED_JSONL_PATH)
+        with CLEANED_JSONL_PATH.open("wb") as f:
+            data_blob.download_to_file(f)
+
+    with jsonlines.open(CLEANED_JSONL_PATH, "r") as f:
+        studies = [study for study in f]
+    return studies
 
 
 def default_progress():
