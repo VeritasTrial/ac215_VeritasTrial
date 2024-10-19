d92de04eb5a7f1af3ddb70b4d01446c98300baf2
Charlie-XIAO <yx2436@nyu.edu>
2024-10-18 21:59:35 -0400

    remove unnecessary printouts

--- src/finetune-model/train.py
+++ src/finetune-model/train.py
@@ -1,6 +1,5 @@
 """The train subcommand."""
 
-import rich
 import vertexai
 from vertexai.tuning import sft
 
@@ -19,7 +18,7 @@ def main(train_dataset, test_dataset, epochs, learning_rate):
 
     # Initialize a new supervised fine-tuning job
     # https://cloud.google.com/vertex-ai/generative-ai/docs/reference/python/latest/vertexai.preview.tuning.sft#vertexai_preview_tuning_sft_train
-    sft_job = sft.train(
+    sft.train(
         source_model=BASE_MODEL,
         train_dataset=f"gs://{BUCKET_NAME}/{BUCKET_FINETUNE_DATA_DIR}/{train_dataset}.jsonl",
         validation_dataset=f"gs://{BUCKET_NAME}/{BUCKET_FINETUNE_DATA_DIR}/{test_dataset}.jsonl",
@@ -28,8 +27,3 @@ def main(train_dataset, test_dataset, epochs, learning_rate):
         adapter_size=4,
         tuned_model_display_name=f"veritas-trial",
     )
-
-    rich.print(f"[bold green]->[/] SFT job created")
-    rich.print(f"   Name:       [dim]{sft_job.tuned_model_name}[/]")
-    rich.print(f"   Endpoint:   [dim]{sft_job.tuned_model_endpoint_name}[/]")
-    rich.print(f"   Experiment: [dim]{sft_job.experiment}[/]")
