479eb71b90bd346dc4971a197d254a89df786a54
Charlie-XIAO <yx2436@nyu.edu>
2024-12-04 13:34:25 -0500

    fix sh command

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -1,9 +1,7 @@
 # Docker entrypoint script
 
-set -ex
 gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
 gcloud config set project "${GCP_PROJECT_ID}"
-gcloud auth configure-docker "${GCP_REGION}-docker.pkg.dev"
-set +ex
+gcloud auth configure-docker "${GCP_REGION}-docker.pkg.dev" --quiet
 
 /bin/bash
