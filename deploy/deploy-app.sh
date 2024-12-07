0e9f2d33a7a4f1a0b29667e954766a0f393c8817
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-12-06 20:20:00 -0500

    ci/cd: add option in workflow to skip image rebuild in deployment (#68)

--- deploy/deploy-app.sh
+++ deploy/deploy-app.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 
-ansible-playbook app/deploy-images.yaml -i inventory.yaml
+usage() {
+  echo "Usage: $0 [--skip-rebuild-images=true|false]"
+  exit 1
+}
+
+SKIP_REBUILD_IMAGES="true" # Skip rebuilding images by default
+
+if [[ $# -gt 1 ]]; then
+  usage
+elif [[ $# -eq 1 ]]; then
+  case "$1" in
+    --skip-rebuild-images=true)
+      SKIP_REBUILD_IMAGES="true"
+      ;;
+    --skip-rebuild-images=false)
+      SKIP_REBUILD_IMAGES="false"
+      ;;
+    *)
+      echo "Invalid argument: $1"
+      usage
+      ;;
+  esac
+fi
+
+if [[ "${SKIP_REBUILD_IMAGES}" == "false" ]]; then
+  ansible-playbook app/deploy-images.yaml -i inventory.yaml
+fi
 CHROMADB_HOST=$(cat chromadb/.instance-ip) ansible-playbook app/deploy-k8s.yaml -i inventory.yaml --extra-vars cluster_state=present
--- deploy/deploy-pipeline.sh
+++ deploy/deploy-pipeline.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 
-ansible-playbook pipeline/deploy-images.yaml -i inventory.yaml
+usage() {
+  echo "Usage: $0 [--skip-rebuild-images=true|false]"
+  exit 1
+}
+
+SKIP_REBUILD_IMAGES="true" # Skip rebuilding images by default
+
+if [[ $# -gt 1 ]]; then
+  usage
+elif [[ $# -eq 1 ]]; then
+  case "$1" in
+    --skip-rebuild-images=true)
+      SKIP_REBUILD_IMAGES="true"
+      ;;
+    --skip-rebuild-images=false)
+      SKIP_REBUILD_IMAGES="false"
+      ;;
+    *)
+      echo "Invalid argument: $1"
+      usage
+      ;;
+  esac
+fi
+
+if [[ "${SKIP_REBUILD_IMAGES}" == "false" ]]; then
+  ansible-playbook pipeline/deploy-images.yaml -i inventory.yaml
+fi
 ./pipeline/pipeline.py
